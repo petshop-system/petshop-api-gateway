563d26d4cd61fb5e8c4b7ee5f3b722042689a7b6
MaironCosta <maironmendes@yahoo.com.br>
2023-12-26 14:09:58 -0300

    corrigindo init.sql

--- configuration/db/init.sql
+++ configuration/db/init.sql
@@ -209,7 +209,7 @@ VALUES ('Rua Jose Bonifácio', 1432);
 INSERT INTO petshop_api.contract (name, email, date_created, fk_id_address, fk_id_person)
 VALUES ('petshop juiz de fora', 'pet_jf@gmail.com', now(), 1, 1);
 
-INSERT INTO petshop_api.phone (number, location, phone_type, fk_id_person)
+INSERT INTO petshop_api.phone (number, code_area, phone_type, fk_id_person)
 VALUES ('912345674', '72', 'celular', 1);
 
 -- first customer
@@ -223,7 +223,7 @@ VALUES ('Rua Lechitz', 11);
 INSERT INTO petshop_api.customer (name, fk_id_address, email, date_created, fk_id_person, fk_id_contract)
 VALUES ('siclano', 2, 'siclano@gmail.com', now(), 2, 1);
 
-INSERT INTO petshop_api.phone (number, location, phone_type, fk_id_person)
+INSERT INTO petshop_api.phone (number, code_area, phone_type, fk_id_person)
 VALUES ('912345000', '72', 'celular', 2);
 
 -- second customer
@@ -237,7 +237,7 @@ VALUES ('38988657000182', 'legal');
 INSERT INTO petshop_api.customer (name, fk_id_address, email, date_created, fk_id_person, fk_id_contract)
 VALUES ('testando cnpj', 3, 'company@gmail.com', now(), 3, 1);
 
-INSERT INTO petshop_api.phone (number, location, phone_type, fk_id_person)
+INSERT INTO petshop_api.phone (number, code_area, phone_type, fk_id_person)
 VALUES ('900045678', '72', 'celular', 3);
 
 -- pet control
